5a7bd3fb99c8950e0c32803a969a70591ac37ca0
Peter Kannewitz <peter.kannewitz@posteo.net>
2024-04-09 13:46:07 +0200

    adds top destination addresses name lookup

--- analysis.R
+++ analysis.R
@@ -1,4 +1,5 @@
-library(tidyverse)
+library("tidyverse")
+library("kableExtra")
 
 nfcapd_data <- read_csv("aggregated_capture.csv",
                          skip = 1,
@@ -24,3 +25,29 @@ ggplot(data = group_by(nfcapd_data, hour, day) %>% summarise(bps = sum(bps)/60/6
     #geom_boxplot() +
     #geom_point(aes(x = hour, y = avg_bps), color = "red") +
     facet_wrap(~day)
+
+ggplot(data = nfcapd_data, mapping = aes(x = ts, y = byt)) +
+    geom_area()
+
+# With which IP Icommunicate with the most?
+
+df_top_hosts <- nfcapd_data %>%
+    group_by(da) %>%
+    summarise(sum_Mbyt = sum(byt)/1000000) %>%
+    slice_max(n = 20, order_by = sum_Mbyt) %>%
+    mutate(domain= map_chr(da, ~ {
+        ns_response <- system(paste("nslookup ", .x), intern=TRUE)
+
+        if (length(ns_response) < 3) {
+            return("None")
+        } else {
+            print(ns_response)
+            tname <- ns_response[str_detect(ns_response, "\tname =")]
+            print(tname)
+            return(str_split(tname, "= ")[[1]][2])
+        }
+    }))
+
+kable(df_top_hosts,
+      format = "latex",
+      col.names = c("Host IP-Address", "MByte Volume", "tname"))
